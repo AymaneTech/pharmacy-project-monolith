0f8b670bb68cf186aa8309607fe52250997f21fd
Aymane El Maini <elmainiaymane03@gmail.com>
2025-03-05 14:33:56 +0000

    add other validations to event dto

--- backend/src/main/java/com/aymanetech/event/event/application/dto/request/EventRequestDto.java
+++ backend/src/main/java/com/aymanetech/event/event/application/dto/request/EventRequestDto.java
@@ -1,20 +1,26 @@
 package com.aymanetech.event.event.application.dto.request;
 
+import com.aymanetech.event.common.application.validation.ReferenceExists;
+import com.aymanetech.event.common.application.validation.UniqueField;
+import com.aymanetech.event.event.domain.entity.Category;
+import com.aymanetech.event.event.domain.entity.Event;
 import com.aymanetech.event.event.domain.vo.BookingType;
+import com.aymanetech.event.event.domain.vo.CategoryId;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record EventRequestDto(@NotBlank String title,
+public record EventRequestDto(@NotBlank @UniqueField(entityClass = Event.class, fieldName = "title", message = "Event title already exists") String title,
                               @NotBlank String description,
-                              @NotNull Integer numberOfSeats,
-                              @NotNull BigDecimal price,
+                              @NotNull @Positive  Integer numberOfSeats,
+                              @NotNull @Positive BigDecimal price,
                               @NotNull LocalDateTime date,
                               @NotBlank String location,
                               BookingType bookingType,
-                              @NotNull Long categoryId,
-                              @NotNull Long userId
+                              @NotNull @ReferenceExists(entityClass = Category.class, idClass = CategoryId.class, message = "Category not found") Long categoryId,
+                              @NotNull Integer userId
 ) {
 }
